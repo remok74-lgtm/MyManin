377336b2cb749d5e16b01013725d372a4334e1bd
remok74-lgtm <remok74@gmail.com>
2025-10-08 19:53:30 +0500

    AM

--- src/MyHomeWork2.java
+++ src/MyHomeWork2.java
@@ -9,7 +9,7 @@ public class MyHomeWork2 {
             int b = 28;
             double c = 15;
             double a = 4 * (b + c - 1) / 2;
-            System.out.println("a= 4*(28 + 15 -1)/2 : " + a);
+            System.out.println("a= 4  *(28 + 15 -1) / 2 : " + a);
         }
 }
 
--- src/lesson_2_hw1/MyHomeWork1.java
+++ src/lesson_2_hw1/MyHomeWork1.java
@@ -7,11 +7,11 @@ public class MyHomeWork1 {
     }
 
     private static void setSumValue() {
-        int n = 24;
-        int a = 2;
-        int b = 4;
-        int ab = (a + b);
-        System.out.println("n= (2 + 4): " +ab);
+        int n =46;
+        double a = 4;
+        double b = 6;
+        double x = (a + b);
+        System.out.println("n= 24 = (2 + 4): " +x);
     }
 
 }
