ad9a62390414c579b8bdab5f90aba414589c35ac
remok74-lgtm <remok74@gmail.com>
2025-10-08 19:11:53 +0500

    AM

---
+++ src/lesson_1_hw/MyHomeWork1.java
@@ -0,0 +1,30 @@
+package lesson_1_hw;
+public class MyHomeWork1 {
+    public static void main(String[] args) {
+        setPersonData();
+    }
+
+    public static void setPersonData() {
+        String name;
+        String surname;
+        int age;
+        float weight;
+        boolean isMaried;
+
+        name = "Ilya";
+        surname = "Mescheryakov";
+        age = 39;
+        weight = 93.7f;
+        isMaried = true;
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Are you Maried? " + isMaried);
+    }
+
+}
+
+
+
+
---
+++ src/lesson_2_hw/MyHomeWork2.java
@@ -0,0 +1,20 @@
+package lesson_2_hw;
+
+public class MyHomeWork2 {
+}
+public class MyHomeWork1 {
+    public static void main(String[] args) {
+        setSumValue();
+    }
+
+    private static void setSumValue() {
+        int ab = 24;
+        double a = 2;
+        double b = 4;
+        double n = (a+b);
+        System.out.println("n= (2 + 4): " +n);
+    }
+
+
+
+}
